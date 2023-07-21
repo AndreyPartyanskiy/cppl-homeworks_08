7faf6530588a14e400a73d3ac727e948170ead8d
Andrey Partyanskiy <andreypartyanskiy@gmail.com>
2023-07-21 22:34:17 +0300

    first commit

---
+++ cppl-homeworks_08_1/cppl-homeworks_08_1/cppl-homeworks_08_1.cpp
@@ -0,0 +1,20 @@
+﻿// cppl-homeworks_08_1.cpp 
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    std::vector <int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "IN: ";
+    for (auto it = vec.begin(); it != vec.end(); it++) std::cout << *it << " ";
+    std::cout << std::endl;
+    std::sort(vec.begin(), vec.end());
+    auto last = std::unique(vec.begin(), vec.end());
+    vec.erase(last, vec.end());
+    std::cout << "OUT: ";
+    for (auto it = vec.begin(); it != vec.end(); it++) std::cout << *it << " ";
+    std::cout << std::endl;
+    return 0;
+}
---
+++ cppl-homeworks_08_2/cppl-homeworks_08_2/cppl-homeworks_08_2.cpp
@@ -0,0 +1,30 @@
+﻿// cppl-homeworks_08_2.cpp 
+
+#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+
+template <class T>
+void print_container(T& in_array)
+{
+    for (auto it = in_array.begin(); it != in_array.end(); it++)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl; 
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);// four one three two. помните почему такой порядок? :)
+    
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // one, two, three, four
+    
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // one, two, three, four
+    
+    return 0;
+}
